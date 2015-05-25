f28f643967ba9d6dd90cceeb7e061a41ed68cee0
Colinator007 <hydragonth@gmail.com>
2015-05-25 12:32:06 -0700

    New Sword

--- src/main/java/com/redcrisisgaming/godsandmortals/item/ItemAresSword.java
+++ src/main/java/com/redcrisisgaming/godsandmortals/item/ItemAresSword.java
@@ -1,5 +1,18 @@
 package com.redcrisisgaming.godsandmortals.item;
 
-public class ItemAresSword {
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemSword;
+import net.minecraftforge.common.util.EnumHelper;
 
+
+public class ItemAresSword extends ItemSword{
+
+	protected static ToolMaterial BRIMSTONE = EnumHelper.addToolMaterial("Brimstone", 3, 1561, 8.0F, 15.0F, 22 );
+	
+	public ItemAresSword(ToolMaterial p_i45356_1_) {
+		super(BRIMSTONE);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
 }
